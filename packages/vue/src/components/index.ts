f86f10e367e21d64723c32e51f3dcc439e7b8d66
Arthur Plazanet <plazanet.art@gmail.com>
2026-04-22 12:23:20 +0200

    Fix lint

--- packages/vue/src/components/index.ts
+++ packages/vue/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as I18nProvider } from './i18nProvider';
-export { default as Translate } from './translate/Translate';
+// export { default as Translate } from './translate/Translate';
+export { default as Tanzlate } from './translate/Tanzlate';
